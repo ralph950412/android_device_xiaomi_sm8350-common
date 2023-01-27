def2bd63360283729be82a838d2b576640dbb75a
ralph950412 <ralph950412@gmail.com>
2023-01-27 20:59:04 +0800

    Revert "sm8350-common: Switch to Google Camera"
    This reverts commit 46f2853aead436a337e0dcc9860f698f479a9aa7.

--- common.mk
+++ common.mk
@@ -26,11 +26,6 @@ $(call inherit-product, vendor/xiaomi/sm8350-common/sm8350-common-vendor.mk)
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
 
-# Inherit Google Camera
-ifeq ($(WITH_GMS),true)
-$(call inherit-product, vendor/xiaomi/venus-gcam/venus-gcam-vendor.mk)
-endif
-
 # AID/fs configs
 PRODUCT_PACKAGES += \
     fs_config_files
@@ -137,7 +132,8 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libgui_vendor
+    libgui_vendor \
+    Snap
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
